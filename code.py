d7df8e394efa5c55c341832c76d25f0f649b1156
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-09-18 18:48:19 +0000

    balbala

---
+++ code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Matsuru Hoshi
+# Created on: Sep 2019
+# This program is the "Hello, World!"
+
+
+def main():
+    # this function prints to the screen and console
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World!")
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass # just a place holder
+
+
+if __name__ == "__main__":
+    main()
